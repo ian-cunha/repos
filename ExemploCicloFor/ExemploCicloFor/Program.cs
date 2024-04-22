884c3d42ed7342e261e5c375ce3da606f64fb175
Ian <iancunha20@gmail.com>
2024-04-22 20:23:38 -0300

    Initial commit

---
+++ ExemploCicloFor/ExemploCicloFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploCicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Insira um número");
+                int num = int.Parse(Console.ReadLine());
+                soma += num;
+            }
+            Console.WriteLine("A soma de rodos os valores inseridos é: "+soma);
+        }
+    }
+}
---
+++ ExemploCicloWhile/ExemploCicloWhile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploCicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, soma = 0;
+
+            do
+            {
+                Console.WriteLine("Insira um número para somar! Insira 0 para parar");
+                num = int.Parse(Console.ReadLine());
+
+                soma += num;
+            } while (num != 0);
+
+            /* while (num != 0)
+            {
+                Console.WriteLine("Insira um número para somar! Insira 0 para parar");
+                num = int.Parse(Console.ReadLine());
+
+                soma += num;
+            }*/
+
+            Console.WriteLine("A soma de todos os números inseridos é: " + soma);
+        }
+    }
+}
