ffda34e38dc365680f30e9612540332f76c3bd21
Ian <iancunha20@gmail.com>
2024-04-22 20:44:46 -0300

    Ex2

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crie um algoritmo que após ler dois números inteiros apresenta a sua soma.
+
+            // ESTRUTURA DADOS
+
+            int n1, n2;
+            //int soma;
+
+            // ALGORITMO
+
+            Console.WriteLine("Insira o primeiro número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            //soma = n1 + n2;
+
+            //Console.WriteLine("A soma de "+n1+" + "+n2+" = " + soma);
+            Console.WriteLine("A soma de " + n1 + " + " + n2 + " = " + (n1 + n2));
+        }
+    }
+}
