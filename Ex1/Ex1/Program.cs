ecf5285864a5af9eab867ff8b2750f7a7dd188e5
Ian <iancunha20@gmail.com>
2024-04-22 20:31:55 -0300

    ex1

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. 
+             * Crie um algoritmo que lê o nome de uma pessoa e escreve "Olá" 
+             * seguido do nome da pessoa. 
+             */
+
+            // ESTRUTURA DADOS
+
+            string nome;
+
+            // ALGORITMO
+
+            Console.WriteLine("Qual o seu nome?");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Olá, " + nome);
+        }
+    }
+}
