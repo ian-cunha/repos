ad9727a715dd3a03f2cefa9fc62a19c87a6f885d
Ian <iancunha20@gmail.com>
2024-04-22 21:57:49 -0300

    Ex3 e Ex4

---
+++ Ex3/Ex3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crie um algoritmo que após ler dois números inteiros apresenta sua diferença.
+
+            // ESTRUTURA DE DADOS
+
+            int n1, n2;
+            // int dif;
+
+            // ALGORITMO
+
+            Console.WriteLine("Insira o primeiro número");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número");
+            n2 = int.Parse(Console.ReadLine());
+
+            //dif = n1 = n2;
+
+            Console.WriteLine("A diferença de " + n1 + " e " + n2 + " é " + (n1 - n2));
+        }
+    }
+}
---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pretende-se lendo a base e altura de um triângulo calcular a sua área.
+            // area = base * altura / 2
+
+            // ESTRUTURA DE DADOS
+
+            double b, altura, area;
+
+            // ALGORITMO
+
+            Console.WriteLine("Qual o valor da base?");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o valor da altura?");
+            altura = double.Parse(Console.ReadLine());
+
+            area = b * altura / 2;
+
+            Console.WriteLine("A área do triângulo é: "+area);
+
+        }
+    }
+}
